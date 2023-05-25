fced6d9fdeac8129e323d1b64095b0eb432f85ee
plla2 <jhbljs92@naver.com>
2023-05-26 01:29:27 +0900

    정적인 Slider 컴포넌트 완성

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import "./Slider.scss"
+import { slider_img_1 } from "../../utils/images"
 
 const Slider = () => {
   return (
-    <div>Slider</div>
+    <div className='hero-slider'>
+      <div className='hero-slider-item'>
+        <img src={slider_img_1} alt="슬라이더 첫번째 이미지" />
+      </div>
+    </div>
   )
 }
 
