68a79310f8682b0b248545fdb9d513b19ca56517
plla2 <jhbljs92@naver.com>
2023-05-26 01:29:10 +0900

    Error, Loader 컴포넌트 제외 Category컴포넌트 완

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import { STATUS } from '../../utils/status'
+import Loader from '../Loader/Loader';
+import Error from '../Error/Error';
+import { Link } from 'react-router-dom';
+import "./Category.scss"
 
-const Category = () => {
+const Category = ({ categories, status }) => {
+  if (status === STATUS.ERROR) return (<Error />);
+  if (status === STATUS.LOADING) return (<Loader />)
+
   return (
-    <div>Category</div>
+    <section className='categories py-5 bg-ghost-white' id='categories'>
+      <div className='container'>
+        <div className='categories-container'>
+          <div className='section-title'>
+            <h3 className='fw-7 text-regal-blue ls-1'>카테고리</h3>
+          </div>
+          <div className='category-items grid'>
+            {categories.map(category => (
+              <Link to={`category/${category.id}`} key={category.id}>
+                <div className='category-item'>
+                  <div className='category-item-img'>
+                    <img src={category.image} alt="카테고리별 이미지" />
+                  </div>
+                  <div className='category-item-name text-center'>
+                    <h6 className='fs-20'>{category.name}</h6>
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
