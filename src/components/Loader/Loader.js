4d34d380f2a33973178de699b6f2a913ef3de24c
plla2 <jhbljs92@naver.com>
2023-05-27 22:34:47 +0900

    로딩 컴포넌트 완성

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { spinner } from '../../utils/images'
+import "./Loader.scss"
 
 const Loader = () => {
   return (
-    <div>Loader</div>
+    <div className='container py-5'>
+      <div className='flex flex-center loader'>
+        <img src={spinner} alt="로딩 사진" />
+      </div>
+    </div>
   )
 }
 
