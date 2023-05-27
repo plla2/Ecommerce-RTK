ab00d0fc9ae81c8449abe5072ae1de497fa4958b
plla2 <jhbljs92@naver.com>
2023-05-27 22:35:46 +0900

    메인페이지에 렌더링될 카테고리와 상품들

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import "./HomePage.scss"
+import Slider from '../../components/Slider/Slider';
+import Category from '../../components/Category/Category';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchCategories, fetchProductsByCategory } from '../../store/categorySlice';
+import SingleCategory from '../../components/SingleCategory/SingleCategory';
+
 
 const HomePage = () => {
+  const dispatch = useDispatch()
+
+  const { data: categories, status: categoryStatus } = useSelector((state) => state.category)
+
+  const { cateProductsAll: productsByCategory, cateProductsAllStatus } = useSelector((state) => state.category)
+
+  useEffect(() => {
+    dispatch(fetchCategories())
+    dispatch(fetchProductsByCategory(1, "all"))
+    dispatch(fetchProductsByCategory(2, "all"))
+  }, [])
+
   return (
-    <div>HomePage</div>
+    <div className='home-page'>
+      <Slider />
+      <Category categories={categories} status={categoryStatus} />
+
+      {/* 첫번째 카테고리 상품들 */}
+      <section>
+        {productsByCategory[0] && <SingleCategory products={productsByCategory[0]} status={cateProductsAllStatus} />}
+      </section>
+
+      {/* 두번째 카테고리 상품들 */}
+      <section>
+        {productsByCategory[1] && <SingleCategory products={productsByCategory[1]} status={cateProductsAllStatus} />}
+      </section>
+    </div>
   )
 }
 
