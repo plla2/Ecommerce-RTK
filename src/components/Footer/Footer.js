ccf261875707c0bd807542f8a1ef4b441f901079
plla2 <jhbljs92@naver.com>
2023-05-30 01:07:24 +0900

    푸터 구현

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div>
+      <footer className="footer py-5 bg-dark">
+        <div className="container">
+          <div className="footer-content text-white grid">
+            <div className="footer-item text-center">
+              <h6 className="fs-17 fw-6">
+                안녕하세요 짱구의 잡화점입니다!! by plla2
+              </h6>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
